1662f28006ac856e08c4f2821b106b470922d449
Tuan le <leminhtuan48198@gmail.com>
2022-08-16 10:37:39 +0700

    test case 0

---
+++ test/AbsoluteNumberCalculatorTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AbsoluteNumberCalculatorTest {
+    @Test
+    void testFindAbsolute0() {
+        int number=0;
+        int expected=0;
+        int result=AbsoluteNumberCalculator.findAbsolute(number);
+        assertEquals(expected,result);
+    }
+
+}
